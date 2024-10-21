230f9f6447bab8c3d8b6a1c57eaad2d18bd11727
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2024-10-21 19:42:16 -0300

    {} > ()
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- pngify.sh
+++ pngify.sh
@@ -4,9 +4,9 @@
 # 1 is input file
 # 2 is output file
 pngify() {
-    INPUT=$(1)
-    OUTPUT=$(2)
-    convert   -alpha remove -alpha off -quality 100 -density 150   -sharpen 0x1.0 -verbose $INPUT $OUTPUT
+    INPUT=${1}
+    OUTPUT=${2}
+    magick convert   -alpha remove -alpha off -quality 100 -density 150   -sharpen 0x1.0 -verbose $INPUT $OUTPUT
 }
 
 pngify $1 $2
